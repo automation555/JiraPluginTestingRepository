bceff3971faebc2c31021898afd970e0ef47c573
GhatnekarPreeti <preeti.ghatnekar@embold.io>
2022-11-25 11:00:08 +0530

    Added folder structure

---
+++ src/Mainclass.java
@@ -0,0 +1,21 @@
+package test;
+
+import java.lang.*;
+
+public class Mainclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "preeti";
+		int length = str.length();
+		int mid = length / 2;
+
+		if (mid % 2 == 0) {
+			String midChar = 
+		}
+		String midChar = (length % 2 != 0) ? (str.substring(0, mid) + str.substring(mid + 1, str.length()))
+				: (str.substring(0, mid - 1) + str.substring(mid + 1, str.length()));
+
+	}
+
+}
